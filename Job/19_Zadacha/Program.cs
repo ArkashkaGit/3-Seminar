2b2b070d3bb57f64e8d91f8d4ba37aec082cd164
Arkashka <sevenkapsul@mail.ru>
2022-07-16 21:49:07 +0300

    19_Zadacha

--- Job/19_Zadacha/Program.cs
+++ Job/19_Zadacha/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу,
+которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+while (true)
+{
+    Console.WriteLine("Введите пятизначное число...");
+    int number = int.Parse(Console.ReadLine());
+    if (number < 99999 && number > 10000)
+    {
+        if (number % 10 == number / 10000 % 10 
+        && number / 10 % 10 == number / 1000 % 10)
+        {
+            Console.WriteLine("данное число ЯВЛЯЕТСЯ палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("данное число НЕ ЯВЛЯЕТСЯ палиндромом");
+        }
+        break;
+    }
+    else
+        Console.WriteLine("Вы ввели не верное число\nпопробуйте снова!!!");
+}
